451988f9166f0286cb6c1ca0e18026af73e184cc
Enrico Brambilla <enric.brambilla@gmail.com>
2021-03-04 10:30:42 +0100

    add png chess pieces cburnett

--- src/it/matlice/matlichess/view/View.java
+++ src/it/matlice/matlichess/view/View.java
@@ -4,23 +4,22 @@ import it.matlice.matlichess.model.Color;
 import it.matlice.matlichess.model.Location;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.util.List;
 
 public class View {
 
-    private ChessboardView chessboardView = new ChessboardView();
+   private ChessboardView chessboardView = new ChessboardView();
 
     public void initialize(){
         new Thread(() -> {
             JFrame frame = new JFrame();
-            frame.setBounds(100, 100, 800, 800);
             frame.setExtendedState(JFrame.NORMAL);
+            frame.setResizable(false);
             frame.setTitle("MatliChess");
             frame.setVisible(true);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.add(chessboardView);
+            frame.pack();
         }).start();
     }
 
@@ -33,7 +32,12 @@ public class View {
         }
     }
 
-    public void makeMove(Location frm, Location to){
-        chessboardView.makeMove(frm, to);
+    public void setPosition(PieceType[][] pieces) {
+        this.chessboardView.setPosition(pieces);
     }
+//
+//    public void makeMove(int from_col, int from_row, int to_col, int to_row) {
+//        chessboardView.makeMove(from_col, from_row, to_col, to_row);
+//    }
+
 }
