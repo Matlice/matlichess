f20305a5fb57a74a3ec02aeb16d0be764d72074d
Enrico Brambilla <enric.brambilla@gmail.com>
2021-03-09 22:10:13 +0100

    right click to cancel move

--- src/it/matlice/matlichess/view/ChessboardView.java
+++ src/it/matlice/matlichess/view/ChessboardView.java
@@ -88,6 +88,10 @@ public class ChessboardView extends JPanel implements MouseListener, MouseMotion
     @Override
     public void mousePressed(MouseEvent e) {
 
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            return;
+        }
+
         Location pointerLoc;
 
         try {
@@ -113,6 +117,23 @@ public class ChessboardView extends JPanel implements MouseListener, MouseMotion
 
     @Override
     public void mouseReleased(MouseEvent e) {
+
+        if (e.getButton() == MouseEvent.BUTTON3) {
+
+            var piece_at = this.pieces
+                    .stream()
+                    .filter(p -> p.getLocation().equals(this.mouse[2] != null ? this.mouse[2] : this.mouse[0]))
+                    .findFirst().orElse(null);
+
+            if (piece_at != null)
+                piece_at.resetOffset();
+
+            resetClicks();
+            this.selected = null;
+
+            this.repaint();
+            return;
+        }
 
         Location pointerLoc;
 
